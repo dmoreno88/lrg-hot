b2b0ff9489fd9515a49ca0831aec8e6514f0602b
Dennis Moreno <dmoreno@911.local>
2020-03-31 16:12:08 -0500

    Under Construction Page

---
+++ src/pages/UnderConstruction.svelte
@@ -0,0 +1,130 @@
+<script>
+     import {onMount} from "svelte";
+     import VideoPlayer from '../components/VideoPlayer.svelte';
+     import Header from "../components/Header.svelte";
+	 import Footer from "../components/Footer.svelte";
+     import Wrapper from "../components/Wrapper.svelte";
+
+
+
+ 
+     
+     let mobileAppear = false;
+
+    onMount(() => {
+
+        mobileAppear = (window.innerWidth < 1290) ? true : false;
+        window.onresize = (e) => {
+            let target = e.target || e.srcElement;
+            if(target.innerWidth < 1290){
+                mobileAppear  = true;
+            }else {
+                mobileAppear = false;
+            }
+        }
+    })
+</script>
+<style>
+    h1 {
+        color: #36454f ;
+        
+    }
+
+    .flex-container {
+        max-width: 1200px;
+     margin: auto;
+     
+     padding: 10px;
+    }
+    img {
+       
+        margin-right: 30px;
+        padding-bottom: 25px;
+    }
+    p {
+        color: #36454f ;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size: 17px;
+        padding-left: 15px; 
+                
+    }
+
+
+
+
+    .grid-container-2{
+        display: grid;
+      
+        grid-template-columns: 50% 50%;
+    }
+    .imgResponsive {
+        width: 590px;
+        height: 375px;
+    }
+    .imgResponsive:hover {
+        cursor: pointer;
+    }
+  
+
+   
+
+
+
+    
+
+    @media only screen and (max-width: 1600px) {
+     
+
+      .center-image{
+          display: flex;
+          text-align: center;
+          align-content: center;
+          justify-content: center;
+      }
+
+       .flex-container {
+            max-width: 100%;
+            margin: 0;
+            padding: 30px;
+           
+        }
+
+    }
+
+     @media only screen and (max-width: 1290px) {
+
+         .grid-container-2{
+             display: flex;
+             flex-direction: column;
+             justify-content: space-around;
+         }
+
+
+       
+     }
+
+   
+</style>
+<Header   />   
+    <Wrapper>
+
+        
+
+<div class="flex-container">
+            
+
+            <h1><center>Sorry, we're doing some work on the site</center></h1>
+                    <div class="grid-container-2">
+                   <div class="center-image">
+                        <img class="imgResponsive" src="/build/assets/pages/pubed/UnderConstruction.jpg" alt="Under Construction" />
+                              <p><b>Thank you for being patient. We are doing some work on the site and will be back shortly.
+                                </b>
+                   </div>
+
+ 
+</div>             
+
+
+
+</Wrapper>
+<Footer />
