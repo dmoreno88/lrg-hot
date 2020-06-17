de2b93bc86f0b05c5c8656c0ad820b401fe57f94
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2020-06-17 15:35:38 -0500

    remove unecessary css

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -71,10 +71,6 @@
   
 }
 
-.mif-menu{
-  font-weight: 800;
-}
-
 img{
   max-width: 100%;
   width: 110px;
@@ -316,7 +312,7 @@ li span {
     
     <div on:click={onShowMenu} class="btnnav">
       <button on:tap={onShowMenu} on:click={onShowMenu}>
-        <span class="mif-menu"></span>
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
       </button>
     </div>
 
