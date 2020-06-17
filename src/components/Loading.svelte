0db16fa528141c9555069e7680058ff00163ec79
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2020-06-17 15:36:06 -0500

    update to the loading component

--- src/components/Loading.svelte
+++ src/components/Loading.svelte
@@ -1,12 +1,53 @@
-<style>
+<script>
+  import {onMount, onDestroy} from "svelte";
+  import {count} from "./store/stores.js";
+  export let progressOn = false;
 
+  let percentage = 0;
+  let unsubscribe;
+  onMount(() => {
+    percentage = 0;
+     unsubscribe = count.subscribe(percent => {
+      if(percent){
+          percentage = percent;
+      }
+    
+    });
+  });
+
+  onDestroy(() =>{
+    unsubscribe();
+    percentage = 0;
+  })
+</script>
+<style>
+  #grid {
+    display: grid;
+    width: 100%;
+    height: 100%;
+    grid-row-gap: 30px;
+    justify-items: center;
+  }
+
+  #progress{
+    background-color: #f5f5f5;
+    width: 80%;
+    margin-top: 20px;
+    height: 20px;
+    
+    
+  }
 .activity-cycle {
-  width: 80px;
-  height: 80px;
+ 
+  width: 140px;
+  height: 140px;
   position: absolute;
   left: 50%;
-  margin-left: -40px !important;
+  margin-left: -70px !important;
   overflow: hidden;
+  background: white;
+  z-index: 2;
+   border: 4px solid #ddd!important;
 }
 .activity-cycle .cycle {
   display: block;
@@ -15,7 +56,7 @@
   top: 50%;
   width: 64px;
   height: 64px;
-  margin: -32px 0 0 -32px;
+  margin: -28px 0 0 -140px;
   border-radius: 50%;
   border: 3px solid transparent;
   border-top-color: #ffffff;
@@ -83,5 +124,16 @@
 
 </style>
 <div id="center" class="color-style activity-cycle">
-    <div class="cycle"></div>
+    <div id="grid">
+      <div class="cycle"></div>
+     {#if progressOn}
+        <div id="progress">
+         <div id="progress-bar" style="width: {percentage}%; background-color: #5cb85c; text-align: center;">
+              {percentage}%
+         </div>
+        </div>
+     {/if}
+     
+    </div>
+    
 </div>
